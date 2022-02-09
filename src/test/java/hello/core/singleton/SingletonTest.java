cd928b60811b1f0e0fc165d6e8cc2b8cbd0767ab
LeeJeongGi <apple4rhk@naver.com>
2022-02-09 21:13:50 +0900

    refactor : 싱글톤 패턴 생성 방법 연습

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -24,4 +24,18 @@ public class SingletonTest {
 
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // same : ==
+        // equal : 자바 문법 equal
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
