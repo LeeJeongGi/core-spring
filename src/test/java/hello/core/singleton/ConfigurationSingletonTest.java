c9fc0f2e83f0dca16938136efff0d944eca8f00c
LeeJeongGi <apple4rhk@naver.com>
2022-02-09 21:53:07 +0900

    refactor : @Configuration과 싱글톤

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository2 = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository = memberService.getMemberRepository();
+        MemberRepository memberRepository1 = orderService.getMemberRepository();
+
+        assertThat(memberRepository).isSameAs(memberRepository1);
+        assertThat(memberRepository).isSameAs(memberRepository2);
+    }
+}
