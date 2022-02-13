0abd5884d254d1f8bf490e4ee4c708b12ce086ba
LeeJeongGi <apple4rhk@naver.com>
2022-02-13 20:54:01 +0900

    refactor : 생성자가 한개일 경우 Autowired 생략 가능

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,6 +13,7 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     @Autowired
+    // 생성자가 1개라면 오토와이어 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
